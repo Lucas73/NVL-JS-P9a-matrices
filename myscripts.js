82521bfc14a93aaacc7c7680c006651be1c10a30
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-05-22 18:16:49 +0200

    matrices exercise

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+var matrix = [[5, 6], [89, 3], [23,56]];
+
+var matrixLength = matrix.length;
+var secondElementLength = matrix[1].length;
+var thirdElement = matrix[2];
+var secondOfThirdElement = matrix[2][1];
+
+console.log('Su longitud es: ' + matrixLength);
+console.log('La longitud de su segundo elemento es: ' + secondElementLength);
+console.log('El valor del tercer elemento es: ' + thirdElement);
+console.log('Y el valor del 2º elemento del tercero es: ' + secondOfThirdElement);
+
+
